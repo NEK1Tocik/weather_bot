64f4e6646f72711113a6f1c36416c274e75c80a0
NEK1Tocik <gold81963@gmail.com>
2024-01-01 21:38:26 +0300

    Initial commit

---
+++ bot_weather.py
@@ -0,0 +1,29 @@
+import telebot
+import requests
+import json
+
+bot = telebot.TeleBot('6834492810:AAHeNiPiF6ZIp7tDJh_zTT_dYDuPI9cqrqI')
+API = '3d9de74844d28377e81415151cbe6a66'
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    bot.send_message(message.chat.id, 'Приветствую, рад тебя видеть! Напиши название города')
+
+
+@bot.message_handler(content_types=['text'])
+def get_weather(message):
+    city = message.text.strip().lower()
+    res = requests.get(f'https://api.openweathermap.org/data/2.5/weather?q={city}&appid={API}&units=metric')
+    if res.status_code == 200:
+       data = json.loads(res.text) # преобразуем в формат json
+       temp = data["main"]["temp"]
+       bot.reply_to(message, f'Сейчас погода: {temp}')
+
+       image = 'sunny.jpg' if temp > 6.0 else 'sun1.png'
+       file = open('./' + image, 'rb')
+       bot.send_photo(message.chat.id, file)
+    else:
+        bot.reply_to(message, 'Город указан не верно!')
+
+
+bot.polling(none_stop=True)
